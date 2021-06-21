e279ba6eaf84ca709a0daae13c2748c7aa2594bf
Marcos <negrosuarez22@gmail.com>
2021-06-21 10:08:21 -0300

    agrego actividad 2 python plus

---
+++ actividad2.py
@@ -0,0 +1,18 @@
+import csv
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+
+archivo=os.path.join(os.getcwd(),'datos_estadisticos.csv')
+archivo_csv=open(archivo,"r")
+
+data_set= pd.read_csv(archivo_csv,encoding='utf-8')
+
+print(data_set)
+
+por_tiempo = data_set.sort_values('Tiempo',ascending=False)
+usuario_tiempo=por_tiempo[['Usuarie - nick','Tiempo']].head(n=3)
+
+print(usuario_tiempo)
+
+usuario_tiempo.plot(kind='bar')
